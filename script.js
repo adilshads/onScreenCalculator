7dd33813396afde19b4885470915964bfff88842
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-05-21 23:11:24 -0400

    Create functions for basic arithmatic

---
+++ script.js
@@ -0,0 +1,24 @@
+// Addition
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Subtraction
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Multiplication
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  // Division
+  function divide(a, b) {
+    if (b === 0) {
+      // Handle division by zero
+      return "Cannot divide by zero!";
+    }
+    return a / b;
+  }
+  
